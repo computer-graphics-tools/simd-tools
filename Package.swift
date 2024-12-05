338957bd470a0ace1b82afb51737504f664b7376
eugene <eugenebokhan@icloud.com>
2024-12-05 17:32:02 +0000

    update deps

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", exact: "510.0.2"),
+        .package(url: "https://github.com/apple/swift-syntax.git", .upToNextMajor(from: "600.0.1")),
     ],
     targets: [
         .macro(
